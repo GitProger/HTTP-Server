2ba783cea94027d16190bd934f77353339e0c386
GitProger <51924999+GitProger@users.noreply.github.com>
2019-09-24 15:32:12 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,29 +6,11 @@
 #include <ctype.h>
 #include <time.h>
 #include <locale.h>
+#include "readconf.h"
 #include "server.h"
-/*#include "client.h"
-#include "bit.h"*/
 
 int main(int argc, char *argv[]) {
+    ReadConfigFile();
     server_init();
-    char ip[16] = "127.0.0.1", port[6] = "80", buffer[16];
-    if (argc >= 3) {
-        strcpy(ip, argv[1]);
-        strcpy(port, argv[2]);
-    } else if (argc == 2) {
-        strcpy(ip, argv[1]);
-        printf("Port (default=%s): ", port);
-        gets_s(buffer, 5);
-        sscanf(buffer, "%s", port);
-    } else if (argc <= 1) {
-        printf("IP address (default=%s): ", ip);
-        gets_s(buffer, 15);
-        sscanf(buffer, "%s", ip);
-        printf("Port (default=%s): ", port);
-        gets_s(buffer, 5);
-        sscanf(buffer, "%s", port);
-    }
-    printf("Server run.\n");
-    return server(ip, port);
+    return server();
 }
