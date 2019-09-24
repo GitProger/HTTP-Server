2469296e96e9d79848bcbc4cdd8fe48b33bfc547
GitProger <51924999+GitProger@users.noreply.github.com>
2019-09-24 15:34:25 +0300

    Update serversys.h

--- serversys.h
+++ serversys.h
@@ -1,70 +1,50 @@
 #ifndef __SERVERSYS_H_INC
 #define __SERVERSYS_H_INC
 #include <stdarg.h>
-char *timeStr(void);
+#include "deepsys.h"
+#include "log.h"
+
 #define SERVER_NAME "Karel"
 #define SERVION_VERSION "0.0b"
-FILE *logFileG;
+#define HTTP_VERSION "1.0"
+#define FULL_SERV_NAME SERVER_NAME"/"SERVION_VERSION
+
 #define MAX_USER_INFO_LEN 0x800
 
-#define LogEvent(text) \
-        { \
-        char *__M_tm_loc_var = timeStr(); \
-        fprintf(logFileG, "[%s] %s\n", __M_tm_loc_var, (text));\
-        free(__M_tm_loc_var); \
+static int readBinFile(const char *fName, byte *buffer, int size) { /* bin */
+    FILE *f = fopen(fName, "rb");
+    int i;
+    if (!f) 
+        return 404;
+    else {
+        for (i = 0; i < size; i++) {
+            fread(buffer + i, 1, 1, f);
         }
-
-#define LogError(text) \
-        { \
-        char *__M_tm_loc_var = timeStr(); \
-        fprintf(logFileG, "[%s] %s | line: %d | function: %s | file: %s\n", __M_tm_loc_var, (text), __LINE__, \
-             __PRETTY_FUNCTION__, __FILE__); \
-        free(__M_tm_loc_var); \
-        }
-
-char *gets_s(char *s, int n) {
-    fflush(stdin);
-    int i, k = getchar();
-    if (k == EOF)
-         return NULL;
-    for (i = 0; (k != EOF && k != '\n') && n--; i++) {
-        s[i] = k;
-        k = getchar();
-        if (k == EOF && !feof(stdin))
-            return NULL;
+        fclose(f);
+        return 200;
     }
-    s[i] = '\0';
-    return s;    
 }
 
-void free_sev_blocks(int am, ...) {
-    va_list l;
-    va_start(l, am);
-    while (am--) 
-        free(va_arg(l, void*));
-    va_end(l);
+static int readTextFile(const char *fName, char *buffer, int size) { /* text */
+    FILE *f = fopen(fName, "rb");
+    /*
+    åñëè "r" à íå "rb" òî
+    êîãäà ôîðìèðóåòñÿ îòâåò ñåðâåðà, òî '\r' íå ñ÷èòàþåòñÿ êàê ñèìâîë, íî
+    îíè âõîäÿò â ðàçìåð ôàéëà, èòîãî, îòïðâëÿåì x áàéò, è èç íèõ n - '\r'
+    òî åñòü ïîñëå òåëà ôàéëà áóäåò åùå n ñëó÷àéíûõ áàéòîâ èç ïàìÿòè 
+    */
+    int i;
+    if (!f) 
+        return 404;
+    else {
+        for (i = 0; i < size; i++) {
+            fread(buffer + i, 1, 1, f);
+        }
+        fclose(f);
+        return 200;
+    }
 }
 
-static int file_size(const char *name) {
-    struct stat f;
-    stat(name, &f);
-    return f.st_size;
-}
-
-static const char *isTwoSgn(int x) {
-    return x < 10 ? "0" : "";
-}
-
-char *makeIP(unsigned int ip_int) {
-    char *ip = (char*)malloc(16);
-    sprintf(ip, "%u.%u.%u.%u", 
-        ((byte*)&ip_int)[0],
-        ((byte*)&ip_int)[1],
-        ((byte*)&ip_int)[2],
-        ((byte*)&ip_int)[3]
-    );
-    return ip;
-}
 
 static bool firstOfApril() {
     time_t t = time(NULL);
@@ -72,34 +52,42 @@ static bool firstOfApril() {
     return (tmCl->tm_mday == 1) && (tmCl->tm_mon + 1 == 4);
 }
 
-char *timeStr() {
-    char *str = (char*)malloc(64);
-    time_t t = time(NULL);
-    struct tm *tmCl = localtime(&t);
-    sprintf(str, "%s%d.%s%d.%d %s%d:%s%d:%s%d", 
-        isTwoSgn(tmCl->tm_mday), tmCl->tm_mday,
-        isTwoSgn(tmCl->tm_mon), tmCl->tm_mon + 1, tmCl->tm_year + 1900,
-        isTwoSgn(tmCl->tm_hour), tmCl->tm_hour,
-        isTwoSgn(tmCl->tm_min), tmCl->tm_min,
-        isTwoSgn(tmCl->tm_sec), tmCl->tm_sec);
-    return str;
+char *getName() {
+    /*const int WSVer = MAKEWORD(2, 2);
+    WSADATA wsaData;*/
+    struct hostent *h;
+    static char buf[128];
+    int i;
+    for (i = 0; i < 128; i++) buf[i] = 0;
+    /*if (!WSAStartup(WSVer, &wsaData)) {
+        if (!gethostname(buf, 128))
+            h = gethostbyname(buf);
+        WSACleanup();
+    }*/
+    if (!gethostname(buf, 128))
+        h = gethostbyname(buf);
+    return buf;
 }
 
 char *__err_page(const char *text, char *resp) {
     char *response_body = malloc(1024);
     if (!response_body) {
-        LogError("Null pointer");
+        LogError("Null pointer", errFileG);
         server_kill();
         return "";
     }
     sprintf(response_body,
         "<html>"
            "<head>"
-              "<title>%s</title>"
+               "<title>%s</title>"
            "</head>"
            "<body>"
-              "<center><h1>%s</h1></center>"
-              "<hr><center>%s</center>"
+               "<center>"
+                  "<h1>"
+                      "%s"
+                  "</h1>"
+               "</center>"
+               "<hr> <center>%s</center>"
            "</body>"
         "</html>",
         text, text, SERVER_NAME);
@@ -114,32 +102,60 @@ char *__err_page(const char *text, char *resp) {
 }
 
 int parseMetReq(char *method, char *param, char *all_info) {
-    char *inf = malloc(MAX_USER_INFO_LEN); 
+    char *inf = malloc(MAX_USER_INFO_LEN);
+    strcpy(inf, all_info);
+    sscanf(all_info, "%s", method);
+    char *ptr = strtok(inf, " ");
+    ptr = strtok(NULL, " ");
+    strcpy(param, inf + strlen(method) + 1);
+    free(inf);
+
+/*    char *inf = malloc(MAX_USER_INFO_LEN); 
     strcpy(inf, all_info);
     char *buffer = strtok(inf, "\r\n");
-    if (!buffer) return 400; /* bad request */
+    if (!buffer) return 400; /* bad request * /
     int i;
     char *bufMet = malloc(64);
     sscanf(buffer, "%s", bufMet);
     buffer = bufMet;
-    if (stricmp(buffer, "GET")
-        || stricmp(buffer, "HEAD")
-        || stricmp(buffer, "POST")
+    if (!stricmp(buffer, "GET")
+        || !stricmp(buffer, "HEAD")
+        || !stricmp(buffer, "POST")
     ) {
         free(inf); free(bufMet);
         return 400;
-    } /* bad request */
+    } /* bad request * /
     strcpy(method, buffer);
     buffer = strtok(NULL, "\r\n");
     if (!buffer) {
         free(inf); free(bufMet);
-        return 400; /* bad request */
+        return 400; /* bad request * /
     }
     strcpy(param, buffer);
     free(inf); free(bufMet);
     return 200; /*ok*/
 }
 
+void GET(const char *);
+void HEAD(const char *);
+void POST(const char *);
+
+void formResponse(char *param, char *response)
+{
+}
+
+void GET(const char *param)
+{
+}
+
+void HEAD(const char *param)
+{
+}
+
+
+void POST(const char *param)
+   {}
+   
 #define ErrorPage(funcName,text) \
     inline char *(funcName)(char *resp) { return __err_page(text, resp); }
 
