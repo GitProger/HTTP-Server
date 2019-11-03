f88d0d92e2b7f57aeada6036e34beb949e725af8
GitProger <51924999+GitProger@users.noreply.github.com>
2019-11-03 16:54:24 +0300

    Create ind.py

---
+++ ind.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+print("Content-Type: text/html")
+print(
+    "<!DOCTYPE HTML>
+    "<html>"
+    "    <head>
+    "        <title> CGI </title>"
+    "    </head"
+    "    <body>"
+    "        <h1> Python CGI here! </h1>"
+    "    </body>"
+    "</html>"
+)
