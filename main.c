b6748acdc6ee16edd2097a6d9c1e0dd279dd8683
GitProger <51924999+GitProger@users.noreply.github.com>
2019-11-03 15:47:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,4 @@
 #include "wide.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-#include <signal.h>
-#include <ctype.h>
-#include <time.h>
-#include <locale.h>
 #include "nets.h"
 #include "server.h"
 
