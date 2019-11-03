c41cbf949b39837b241bfaf27f2f6936e5699cd1
GitProger <51924999+GitProger@users.noreply.github.com>
2019-11-03 15:23:25 +0300

    Update server.h

--- server.h
+++ server.h
@@ -3,9 +3,6 @@
 
 /*#define _WIN32_WINNT 0x501*/
 #include <string.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <windows.h>
 #include <sys/stat.h>
 #include <signal.h>
 void server_kill(void);
@@ -31,14 +28,15 @@ void sigsegv(int sig) {
 }
 
 int server_init() {
+    ReadConfigFile();
     signal(SIGSEGV, sigsegv);
     signal(SIGINT, sigint);
     setlocale(LC_ALL, "Russian");
-    InitLogsFiles();
+    InitLogsFiles();   
     return 0;
 }
 
-int showLog(const char *all, int ip) {
+static int showLog(const char *all, int ip) {
     char *buffer = (char*)malloc(MAX_USER_INFO_LEN);
     if (!buffer) {
         LogError("Null pointer", logFileG);
@@ -47,11 +45,12 @@ int showLog(const char *all, int ip) {
     strncpy(buffer, all, MAX_USER_INFO_LEN);
     const char *reqt = strtok(buffer, ":\r\n");
     if (!reqt) reqt = "Unknown";
-    char *host = makeIP(ip);
+    char *cli = makeIP(ip);
     char *tm = timeStr();
-    fprintf(logFileG, "[%s] request from: %15s | %s\n", tm, host, reqt);
+    fprintf(logFileG, "[%s] request from: %15s | %s\n", tm, cli, reqt);
+    fflush(logFileG);
     free(buffer);
-    free(host);
+    free(cli);
     free(tm);
     return 0;
 }
@@ -61,11 +60,15 @@ int server() {
     char *time = timeStr();
     if (!time)
         time = " Error in timeStr()";
-    WSADATA wsaData;
-    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    /*WSADATA wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);*/
+    int result = startup();
+    
     fprintf(logFileG, "[%s] Entry to the main server proc (IP=%s, Port=%s)\n", time, ServerConfig.ip, ServerConfig.port);
+    fflush(logFileG);
     if (result) {
         fprintf(errFileG, "[%s] WSAStartup(...) failed: %d\n", time, result);
+        fflush(errFileG);    
         return result;
     }
     struct addrinfo *addr = NULL;
@@ -81,65 +84,72 @@ int server() {
 
     if (result != 0) {
         fprintf(errFileG, "[%s] getaddrinfo(...) failed: %d\n", time, result);
-        WSACleanup();
+        fflush(errFileG);
+        cleanup();
         return 1;
     }
 
     int listen_socket = socket(addr->ai_family, addr->ai_socktype,
         addr->ai_protocol);
     if (listen_socket == INVALID_SOCKET) {
-        fprintf(errFileG, "[%s] Error at socket: %d\n", time, WSAGetLastError());
+        fprintf(errFileG, "[%s] Error at socket: %d\n", time, lasterr());
+        fflush(errFileG);
         freeaddrinfo(addr);
-        WSACleanup();
+        cleanup();
         return 1;
     }
 
     result = bind(listen_socket, addr->ai_addr, (int)addr->ai_addrlen);
 
     if (result == SOCKET_ERROR) {
-        fprintf(errFileG, "[%s] bind(...) failed with error: %d\n", time, WSAGetLastError());
+        fprintf(errFileG, "[%s] bind(...) failed with error: %d\n", time, lasterr());
+        fflush(errFileG);
         freeaddrinfo(addr);
         closesocket(listen_socket);
-        WSACleanup();
+        cleanup();
         return 1;
     }
 
     if (listen(listen_socket, SOMAXCONN) == SOCKET_ERROR) {
         fprintf(errFileG, "[%s] listen(...) failed with error: %d\n",
-            time, WSAGetLastError());
+            time, lasterr());
+        fflush(errFileG);
         closesocket(listen_socket);
-        WSACleanup();
+        cleanup();
         return 1;
     }
 
 
     int client_socket = INVALID_SOCKET;
     char *user_info = (char*)malloc(MAX_USER_INFO_LEN); /* request of client */
-    char *response = (char*)malloc(1 << 25); /* 1MB */
+    char *response = (char*)malloc((1 << 20) * ServerConfig.heap_sz_mb); /* 1MB * heap_sz_mb */
     response[0] = 0;
     if (!response) {
         LogError("No free memory.", errFileG);
         closesocket(listen_socket);
-        WSACleanup();
+        cleanup();
         return 1;
     }
     
     fprintf(logFileG, "[%s] Run (IP=%s, Port=%s)\n", time, ServerConfig.ip, ServerConfig.port);
+    fflush(logFileG);
     /* MAIN */
     for (;;) {
         client_socket = accept(listen_socket, NULL, NULL);
         
         if (client_socket == INVALID_SOCKET) {
-            fprintf(stderr, "accept(...) failed: %d\n", WSAGetLastError());
+            fprintf(errFileG, "accept(...) failed: %d\n", lasterr());
+            fflush(errFileG);
             closesocket(listen_socket);
-            WSACleanup();
+            cleanup();
             return 1;
         }
 
         result = recv(client_socket, user_info, MAX_USER_INFO_LEN, 0);
 
         if (result == SOCKET_ERROR) {
-            fprintf(logFileG, "[%s] recv(...) failed: %d\n", time, result);
+            fprintf(errFileG, "[%s] recv(...) failed: %d\n", time, result);
+            fflush(errFileG);
             closesocket(client_socket);
         } else if (result == 0) {
             LogError("Connection closed.", errFileG);
@@ -174,17 +184,16 @@ int server() {
           SendAnswer:
             result = send(client_socket, response, resplen, 0);
             if (result == SOCKET_ERROR) {
-                fprintf(errFileG, "[%s] send(...) failed: %d\n", time, WSAGetLastError());
+                fprintf(errFileG, "[%s] send(...) failed: %d\n", time, lasterr());
+                fflush(errFileG);
             }
             closesocket(client_socket);
         }
     }
-    free(response);
-    free(user_info);
-    free(time);
+    free_sev_blcks(3, response, user_info, time);
     closesocket(listen_socket);
     freeaddrinfo(addr);
-    WSACleanup();
+    cleanup();
     return 0;
 }
 
