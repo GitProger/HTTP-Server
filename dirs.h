afeccc6a340b2b47e6ed52af4df822ce2b9d503f
GitProger <51924999+GitProger@users.noreply.github.com>
2019-11-03 17:01:15 +0300

    Create dirs.h

---
+++ dirs.h
@@ -0,0 +1,27 @@
+#ifndef __DIRS_H_INC
+#define __DIRS_H_INC
+#include <windows.h>
+
+typedef struct file_list {
+    char **names;
+    int n;
+} file_list;
+
+static file_list getList(const char *dir) {
+    file_list res = {.names = NULL, .n = 0};
+    
+    return res;
+}
+
+static void free_list(file_list x) {
+    int i = 0;
+    for (i = 0; i < x.n; i++)
+        free(x.names[i]);
+    free(x.names);
+}
+
+char *list_page_m() {
+
+}
+
+#endif
