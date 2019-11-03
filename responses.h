0b4bdde26c2cd023ca9e896d3cf8e9692b136594
GitProger <51924999+GitProger@users.noreply.github.com>
2019-11-03 17:00:25 +0300

    Update responses.h

--- responses.h
+++ responses.h
@@ -1,6 +1,7 @@
 #ifndef __RESPONSES_H_INC
 #define __RESPONSES_H_INC
 #include "makecontent.h"
+#include "dirs.h"
 ErrorPage(badRequest , "400 Bad Request"           );
 ErrorPage(authReq    , "401 Authorization Required");
 ErrorPage(forbidden  , "403 Forbidden"             );
@@ -8,23 +9,14 @@ ErrorPage(notFound   , "404 Not Found"             );
 ErrorPage(Imateapot  , "418 I`m a teapot"          );
 ErrorPage(intServErr , "500 Internal Server Error" );
 
-
-void MakeHeader(char *resph, const char *ver, const char *status,
-        const char *contenttype, int contentlen/*, const char *accranges*/
-    ) {
-    sprintf(resph,
-        "HTTP/%s %s\r\nVersion: HTTP/%s\r\nContent-Type: %s\r\n"
-        "Content-Length: %d\r\nAccept-Ranges: bytes\r\n\r\n",
-        ver, status, ver, contenttype, contentlen
-        );
-    /*MakeHeader(resph, "1.0", "200 OK", "image/gif", 132435, bytes)*/
-}
-
+int do_GET(int, char *, char *, char *);
+int do_HEAD(int, char *, char *, char *);
 
 int CreateResponse(const char *method, char *uri, char *response) {
     int i, resplen = 0;
-    for (i = 0; i < strlen(uri); i++) /* remove '/' in file name beginnig */
-        uri[i] = uri[i + 1];
+    if (uri[0] == '/')
+        for (i = 0; i < strlen(uri); i++) /* remove '/' in file name beginnig */
+            uri[i] = uri[i + 1];
 
     if (!stricmp(uri, ""))
         uri = ServerConfig.index;
@@ -49,35 +41,44 @@ int CreateResponse(const char *method, char *uri, char *response) {
         MakeHeader(response, HTTP_VERSION, "200 OK", MIME, sz);        
         return strlen(response);
     } else if (!stricmp(method, "GET")) {
-    
-        if (sz == -1) {
-            notFound(response);
-            return strlen(response);
-        }
-        char *buffer = malloc(sz + 1);
-        getGeneralMIME(MIME, gen);
-        if (!stricmp(gen, "text")) {/* text */
-            readTextFile(uri, buffer, sz);
-        } else /* bin */
-            readBinFile(uri, buffer, sz);
-
-        MakeHeader(response, HTTP_VERSION, "200 OK", MIME, sz);
-        resplen = strlen(response);
-        for (i = 0; i < sz; i++)
-            response[resplen + i] = buffer[i];
-        resplen += sz;
-        response[resplen] = 0;
-
-        free(buffer);
-        return resplen;
-                   
+        return do_GET(sz, uri, MIME, response);
     } else if (!stricmp(method, "POST")) {
-        MakeHeader(response, HTTP_VERSION, "200 OK", "text/plain", 0);
-        return strlen(response);
+        return do_POST(sz, uri, MIME, response);
     } else {
         badRequest(response);
         return strlen(response);
     }
 }
+            
+int do_GET(int sz, char *param, char *MIME, char *response) {
+    if (sz == -1) {
+        notFound(response);
+        return strlen(response);
+    }
+    int resplen = 0, i;
+    char *buffer = malloc(sz + 1), *gen = malloc(64);
+    getGeneralMIME(MIME, gen);
+    if (!stricmp(gen, "text")) {/* text */
+        readTextFile(param, buffer, sz);
+    } else /* bin */
+        readBinFile(param, buffer, sz);
 
+    MakeHeader(response, HTTP_VERSION, "200 OK", MIME, sz);
+    resplen = strlen(response);
+    for (i = 0; i < sz; i++)
+        response[resplen + i] = buffer[i];
+    resplen += sz;
+    response[resplen] = 0;
+    free_sev_blcks(2, buffer, gen);
+    return resplen;
+}
+
+int do_POST(int sz, char *param, char *MIME, char *response) {
+    return do_GET(sz, param, MIME, response);
+}
+
+int do_HEAD(int sz, char *param, char *MIME, char *response) {
+    MakeHeader(response, HTTP_VERSION, "200 OK", MIME, sz);        
+    return strlen(response);
+}
 #endif
