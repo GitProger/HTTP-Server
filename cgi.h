62e92eea14700eb8da19c9235c6320ed9a328ac0
GitProger <51924999+GitProger@users.noreply.github.com>
2019-11-08 18:53:07 +0300

    Update cgi.h

--- cgi.h
+++ cgi.h
@@ -1,14 +1,21 @@
 #ifndef __CGI_H_INC
 #define __CGI_H_INC
 
-static const char *cgiext[] = {
-    "cgi", "py", "exe"
-}
-
-void run_cgi(const char *req) {
-    if (!ServerConfig.cgi)
-        return;
-    /*...*/
+char *run_cgi(const char *req, char *resp) {
+    char *cmd = calloc(1024, 1);
+    char *out = calloc(1024, 1);
+    char *mime = calloc(1024, 1);
+    sprintf(out, "outp.x");
+    strcat(cmd, "cgi.exe \"");
+    strcat(cmd, req);
+    strcat(cmd, "\"");
+    system(cmd);
+    readTextFile(out, resp, file_size(out));
+    free(cmd);
+    free(out);
+   /*TODO mime!!!*/
+    free(mime);
+    return "text/html";
 }
 
 #endif
