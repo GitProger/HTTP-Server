dcda1529caed5763af46f61baf111e5c8a74bc4d
GitProger <51924999+GitProger@users.noreply.github.com>
2019-11-09 12:07:34 +0300

    Create ind.pl

---
+++ ind.pl
@@ -0,0 +1,15 @@
+#!C:\Perl\perl.exe
+
+use strict
+use warning
+
+sub main {
+     say "<html>";
+     say "    <head> <title> Perl here </title> </head>";
+     say "    <body>";
+     say "        <h1> Hello from Perl CGI </h1>";
+     say "    </body>";
+     say "</html>";
+}
+
+main();
